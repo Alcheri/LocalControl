47472111e9abb4d0cc30342a3649a6dc7eaedd51
Barry KW Suridge <barry@example.com>
2026-04-25 14:35:43 +1000

    Added Codex file

--- test.py
+++ test.py
@@ -5,11 +5,25 @@
 #
 ###
 
-from supybot.test import *
+import unittest
+
+from supybot import test as supybot_test
+
+try:
+    from . import plugin
+except ImportError:  # pragma: no cover - allows direct pytest execution.
+    import plugin
 
 
-class LocalControlTestCase(PluginTestCase):
+class LocalControlTestCase(supybot_test.PluginTestCase):
+    __test__ = False
+
     plugins = ("LocalControl",)
+
+
+class TestLocalControlModule(unittest.TestCase):
+    def test_plugin_class_is_available(self):
+        self.assertIs(plugin.Class, plugin.LocalControl)
 
 
 # vim:set shiftwidth=4 tabstop=4 expandtab textwidth=79:
